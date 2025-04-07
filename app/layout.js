3e8f2f66432e450b8cdaaa948a2be9800d4eccb6
oliverudekyll <oliverudekll@gmail.com>
2025-04-07 10:22:28 +0300

    added magenta c favicon

--- app/layout.js
+++ app/layout.js
@@ -36,20 +36,25 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link
-          rel="icon"
-          href={`/favicons/favicon-${initialFavIconIndex}.ico`}
-          sizes="any"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
         />
         <link
-          rel="apple-touch-icon"
-          href="/favicons/apple-touch-icon.png"
-          sizes="any"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicon/site.webmanifest" />
       </head>
-      <body className={`${alpinaTypeWriter.variable}`}>
-        <DynamicFavicon />
-        {children}
-      </body>
+      <body className={`${alpinaTypeWriter.variable}`}>{children}</body>
     </html>
   );
 }
